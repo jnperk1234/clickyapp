394909f57ed7bd8a0bd8a1a5fa918dd6399cd7c3
Jessica Perkins <jnperk1234@gmail.com>
2018-06-28 23:49:15 -0500

    final

--- src/components/GameGrid/GameGrid.js
+++ src/components/GameGrid/GameGrid.js
@@ -1,12 +1,13 @@
 import React from "react";
-import OnePButton from "../OnePButton";
+import CatButton from "../OnePButton";
 
 const GameGrid = (props) => (
-    <div id="game-grid" className="container justify-content-center">
-        {props.op.map((op) => (
-            <OnePButton key={op.id} id={op.id} clickOP={props.clickOP}></OnePButton>
-        ))}
-    </div>
+
+<div id="game-grid" className="container justify-content-center">
+    {props.onep.map((op) => (
+        <OnePButton key={op.id} id={op.id} clickOp={props.clickOp}></OnePButton>
+    ))}
+</div>
 );
 
-export default GameGrid
+export default GameGrid;
--- src/components/NavBar/Navbar.js
+++ src/components/NavBar/Navbar.js
@@ -3,7 +3,7 @@ import Score from "../Score";
 
 const Navbar = (props) => (
     <nav className="navbar navbar-expand-lg navbar-light bg-dark justify-content-between">
-        <span className="navbar-brand" id="title">COne Piece Clicker</span>
+        <span className="navbar-brand" id="title">One Piece Clicker</span>
         {/* Re-enable for mobile responsiveness */}
         {/* <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
